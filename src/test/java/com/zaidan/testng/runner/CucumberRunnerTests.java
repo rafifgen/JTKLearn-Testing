d0d1ca45b34bf83121e38e50e8de3abe00e5c1d3
Reqi Jumantara Hapid <reqi.jumantara@gmail.com>
2025-07-07 15:57:43 +0700

    [Add] TC FR-11 Quiz Results History Completed

--- src/test/java/com/zaidan/testng/runner/CucumberRunnerTests.java
+++ src/test/java/com/zaidan/testng/runner/CucumberRunnerTests.java
@@ -3,7 +3,7 @@ package com.zaidan.testng.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@QuizHistory", features = "src/test/resources/features", glue = "com.zaidan.testng.definitions", plugin = {
+@CucumberOptions(tags = "", features = "src/test/resources/features", glue = "com.zaidan.testng.definitions", plugin = {
         "pretty", "json:target/cucumber/report_test.json",
         "html:target/report_tes.html" })
 
